36abc74517359768040cbd272ee016dd598181f5
daudu <jdaudu@digicoreltd.com>
2026-09-19 16:05:17 +0100

    feat(common/utils): add utils to generate OTPs + create/compare hash string + parse list

--- libs/swet-common-lib/src/utils/token.util.ts
+++ libs/swet-common-lib/src/utils/token.util.ts
@@ -1,3 +1,5 @@
+import { argon2id, hash, verify } from 'argon2';
+import { randomFillSync } from 'crypto';
 import { customAlphabet } from 'nanoid';
 import { uuidv7 } from 'uuidv7';
 
@@ -12,5 +14,28 @@ export class TokenUtil {
   static generateIdentifier(prefix = ''): string {
     const id = customAlphabet(TokenUtil.UPPER_ALPHA_NUMERIC, 18)();
     return `${prefix}${id}`;
+  }
+
+  static generateOTP(length: number = 6): string {
+    const digits = '0123456789';
+    return Array.from(randomFillSync(new Uint8Array(length)))
+      .map((byte) => digits[byte % digits.length])
+      .join('');
+  }
+
+  static async createHashString(plainText: string): Promise<string> {
+    return hash(plainText, {
+      type: argon2id,
+      memoryCost: 64 * 1024,
+      timeCost: 3,
+      parallelism: 1,
+    });
+  }
+
+  static async compareHashString(
+    plain: string,
+    hash: string,
+  ): Promise<boolean> {
+    return verify(hash, plain);
   }
 }
